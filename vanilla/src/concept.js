90342389d92e6fd1f1e38d0b56567b6473f4b68d
iamsg97 <iamsg97@outlook.com>
2025-10-04 14:25:33 +0000

    feat: added pagination generator example

--- vanilla/src/concept.js
+++ vanilla/src/concept.js
@@ -40,3 +40,30 @@ function* poweredGen() {
 const gen = poweredGen()
 console.log(gen.next())
 console.log(gen.next('Twinkle'))
+
+// Attempting dynamic pagination
+function* paginate(arr, size) {
+    let i = 0
+    while (i < arr.length) {
+        yield arr.slice(i, i + size) // pick from i to excluding i + size
+        i += size
+    }
+}
+
+const items = [10, 20, 30, 40, 50, 60, 70]
+
+/**
+ * @description look at the below example to understand how it actually works
+ * @example
+ * ```js
+ * 	const generatedPagesIterator = paginate(items, 3)
+ * 	while (!generatedPagesIterator.done) {
+ *		// here generatedPagesIterator.next() moves the iterator forward
+ *		// here generatedPagesIterator.next().value will yield the value (and wait at the next)
+ *		// here generatedPagesIterator.done lets us know if the iterator has finished
+ *		console.log(`group of pages to be available for pagination: ${generatedPagesIterator.next().value}`)
+ * 	}
+ */
+for (let page of paginate(items, 3)) {
+    console.log(page)
+}
